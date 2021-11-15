bf9572fdb52a2c0b121a46e9f7f86ce317bf67f0
kevinmr59 <kevinmura59610@gmail.com>
2021-11-15 10:43:34 +0100

    switch

---
+++ add.js
@@ -0,0 +1,27 @@
+let verif =prompt("nom accesoire");
+switch(verif)
+{
+    case 'souris':
+        alert("j'utilise une souris");
+        break;
+    case 'clavier':
+        alert("j'utilise un clavier");
+        break;
+    case 'ordinateur':
+        alert("j'utilise un ordinateur")
+        break;
+    case 'tapis':
+        alert("j'utilise un tapis")
+        break;
+    case 'imprimate':
+        alert("j'utilise une imprimante")
+        break;
+    case 'ondulateur':
+        alert("j'utilise un ondulateur")
+        break;
+    default:
+        alert("inconnu")
+
+
+
+}
